e95bd22ecf7df1add14698a73f5f4fab45392136
dcbryant <cong.ding@100credit.com>
2020-02-16 23:03:54 +0800

    通过promiseA+规范

--- promise/history3/promise1.js
+++ promise/history3/promise1.js
@@ -2,11 +2,89 @@ const SUCCESS = 'fulfilled'
 const FAIL = 'rejected'
 const PENDING = 'pending'
 
+// Object.defineProperty(obj, 'then', {
+//   get() {
+//     if() {
+//       throw new Error
+//     }
+//     throw new Error()
+//   }
+// })
+
 // promsie2: 返回的新的promise
 // x: then方法的返回值
-function resolvePromise(promsie2, x, resolve, reject) {
-  if (promsie2 === x) {
-    return reject(new TypeError('Chaining cycle detected for promise'))
+
+// function resolvePromise(promsie2, x, resolve, reject) {
+//   if (promsie2 === x) {
+//     return reject(new TypeError('Chaining cycle detected for promise'))
+//   }
+//   // 判断x的类型
+//   // promsie有n种实现，都符合了这个规范
+//   let called
+//   // 怎么判断x是不是一个promsie，看他有没有then方法
+//   if (typeof x === 'function' || (typeof x === 'object' && x != null)) {
+//     // 应该判断别人的promise成功了就不能失败，失败了就不能成功
+//     try {
+//       let then = x.then
+//       if (typeof then === 'function') { //认为它是一个promise
+//         // 不要使用x.then否则会在此取值
+
+//         // then.call(x,y => {
+//         //   // 如果promise是成功的话就把结果往下传
+//         //   // 如果失败的话就让下一个也失败
+//         //   // resolve(y)
+//         //   // 递归调用，防止promise里面还返回promise
+//         //   if (called) return
+//         //   called = true
+//         //   resolvePromise(promise2, y, resolve, reject)
+//         // }, r => {
+//         //   if (called) return
+//         //   called = true
+//         //   reject(r)
+//         // })
+//       } else {
+//         // 普通值不用管，resolve后不会再调用reject
+//         resolve(x)
+//       }
+//     } catch (e) {
+//       if (called) return
+//       called = true
+//       reject(e)
+//     }
+//   } else { //普通值
+//     // 普通值不用管，resolve后不会再调用reject
+//     resolve(x)
+//   }
+// }
+
+function resolvePromise(promise2, x,resolve,reject) { 
+  if(promise2 === x){
+     return reject(new TypeError('TypeError: Chaining cycle detected for promise #<Promise>'));
+  }
+  let called;
+  if(typeof x === 'function' || (typeof x === 'object' &&  x != null)){
+    try{
+      let then = x.then;  // then 可能是getter object.defineProperty
+      if(typeof then === 'function'){  // {then:null}
+         then.call(x,y=>{ 
+           if(called) return; // 1)
+           called = true;
+            resolvePromise(promise2,y,resolve,reject); 
+         },r=>{
+           if(called) return; // 2)
+           called = true;
+            reject(r);
+         }) 
+      }else{ 
+        resolve(x);
+      }
+    }catch(e){
+      if(called) return; // 3) 为了辨别这个promise 不能调用多次
+      called = true;
+      reject(e);
+    }
+  }else{
+    resolve(x);
   }
 }
 
@@ -34,14 +112,21 @@ class Promise {
       }
     }
 
-    executor(resolve, reject)
+    try {
+      executor(resolve, reject)
+    } catch (error) {
+      reject(error)
+    }
   }
 
   then(onFulfilled, onRejected) {
+    // 值的穿透
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : (val) => val;
+    onRejected = typeof onRejected === 'function' ? onRejected : (err) => {throw err}
     let promise2
     // promise不停的调用then方法，返还了一个新的promise
     promise2 = new Promise((resolve, reject) => {
-      if (this.status === 'fulfilled') {
+      if (this.status === SUCCESS) {
         // onFulfilled(this.value)
         // 为了获取到promise2这个值，因此外层得加setTimeout
         setTimeout(() => {
@@ -53,10 +138,10 @@ class Promise {
           } catch (error) {
             reject(error)
           }
-        }, 0);
+        });
       }
   
-      if(this.status === 'rejected') {
+      if(this.status === FAIL) {
         // onRejected(this.reason)
         setTimeout(() => {
           try {
@@ -65,7 +150,7 @@ class Promise {
           } catch (error) {
             reject(error)
           }
-        }, 0);
+        });
       }
 
       if(this.status === PENDING) {
@@ -96,8 +181,17 @@ class Promise {
     })
     return promise2
   }
+}
 
-
+// promises-aplus-tests
+// deffered
+Promise.defer = Promise.deferred = () => {
+  let dfd = {}
+  dfd.promise = new Promise((resolve, reject) => {
+    dfd.resolve = resolve
+    dfd.reject = reject
+  })
+  return dfd
 }
 
 module.exports = Promise
